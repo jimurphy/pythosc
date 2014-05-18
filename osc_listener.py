d419b3bd843e8a05cf7216cd8551173b60ce5959
JimMurphy <jim.w.murphy@gmail.com>
2014-05-18 19:15:53 +1200

    Slight progress on unpacking.

--- osc_listener.py
+++ osc_listener.py
@@ -8,60 +8,68 @@ import struct
 class EchoUDP(DatagramProtocol):
     
     def datagramReceived(self, data, address):
+        
+        def unpackInt(position, datagram):
+            packedInt = datagram[0]
+            for num in range(1,4):
+                print "NUM", num
+                packedInt = packedInt + datagram[num]
+            print packedInt
+            intValue = struct.unpack(">i", packedInt)
+            print "Unpacked Integer:", intValue #output is tuple
+            #now we need to delete this from overall message
+            
+        def unpackFloat(position, datagram):
+            print "unpacking float at position", position
+
+        def unpackString(position, datagram):
+            print "unpacking string at position", position
+            nullRegex = re.sub(r',','',datagram) #Remove comma
+            nullRegex = re.sub(r'\0{1,}','/',nullRegex) #Many nulls to one slash
+            oscValues = nullRegex.split("/") # /x/y/z into [x, y, z]
+            del oscValues[0] # Unsure why.
+            
+            stringValue = oscValues[position]
+            print "STRING VALUE AT ", position, ":", stringValue
+        
+        datagramlist = data
         numberOfInts = 0
         numberOfFloats = 0
         numberOfStrings = 0
-        
+                
         typeTagRegex = re.compile('\,[\S]*?\0') #Not sure why these need
         addressRegex = re.compile('\/[\S]*?\,')  #to be compiled every time...
 
-        matchOscAddress = re.match(addressRegex, data, flags=0)
-        searchOscTypetag = re.search(typeTagRegex, data, flags=0)
+        matchOscAddress = re.match(addressRegex, datagramlist, flags=0)
+
+        searchOscTypetag = re.search(typeTagRegex, datagramlist, flags=0)
 
         if matchOscAddress:
             matchOscAddress = re.sub(',','',matchOscAddress.group())
             print "Address: ",matchOscAddress
-            data = data.replace(matchOscAddress, '')
+            datagramlist = datagramlist.replace(matchOscAddress, '')
         
         if searchOscTypetag:
-            searchOscTypetag = re.sub(r',','',searchOscTypetag.group())
+            searchOscTypetag = searchOscTypetag.group()
             print "Typetag: ", searchOscTypetag
-            typeTagLength = (len(searchOscTypetag)-1)
-            data = data.replace(searchOscTypetag, '')                   
+            typeTagLength = (len(searchOscTypetag)-2)
             print typeTagLength
+            
+            datagramlist = datagramlist.replace(searchOscTypetag, '')
             for num in range(0, typeTagLength):
                 if searchOscTypetag[num] == 'i':
                     print "INT TYPE FOUND AT", num
                     numberOfInts = numberOfInts + 1
-                    self.unpackInt(num, data)
-                elif searchOscTypetag[num] == 'f':
+                    unpackInt(num, datagramlist)
+                if searchOscTypetag[num] == 'f':
                     print "FLOAT TYPE FOUND AT", num
                     numberOfFloats = numberOfFloats + 1
-                    self.unpackFloat(num, data)
-                elif searchOscTypetag[num] == 's':
+                    unpackFloat(num, datagramlist)
+                if searchOscTypetag[num] == 's':
                     print "STRING TYPE FOUND AT", num
                     numberOfStrings = numberOfStrings + 1
-                    self.unpackString(num, data)
+                    unpackString(num, datagramlist)
         
-        nullRegex = re.sub(r',','',data) #Remove comma
-        nullRegex = re.sub(r'\0{1,}','/',nullRegex) #Many nulls to one slash
-        oscValues = nullRegex.split("/") # /x/y/z into [x, y, z]
-        del oscValues[0] # Unsure why.
-        
-        paddedValue = oscValues[2]
-        padAmount = 4 - (len(oscValues[2]) % 4)
-        paddedValue = ("\0" * padAmount) + paddedValue
-        struct.unpack(">i", paddedValue)
-        
-    def unpackInt(self, position, datagram):
-        print "unpacking int at position", position
-
-    def unpackFloat(self, position, datagram):
-        print "unpacking float at position", position
-
-    def unpackString(self, position, datagram):
-        print "unpacking string at position", position
-
 
 def main():    
     reactor.listenUDP(8009, EchoUDP())
--- osc_tester.py
+++ osc_tester.py
@@ -4,13 +4,13 @@ import osc_sender
 def sendOsc():
     sender1 = osc_sender.OSCClient("127.0.0.1", 8009)
     sender1.SetAddress("/test/message")
-    sender1.SetTypetag("ssi")
+    sender1.SetTypetag("is")
     
     #sender1.AddString("")
     #sender1.AddFloat(69.5)
-    sender1.AddString("yolo")
+   # sender1.AddString("yolo")
+    sender1.AddInt(121)    
     sender1.AddString("giggle")
-    sender1.AddInt(999)
     sender1.Transmit()
 
 while True:
